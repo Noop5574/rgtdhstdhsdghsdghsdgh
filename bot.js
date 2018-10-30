d85d976bc315f0a5bf8c37b0770dc9abd1c442f9
Noop5574 <44319316+Noop5574@users.noreply.github.com>
2018-10-30 15:29:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("q",{type: 'WATCHING'})
+   client.user.setActivity("Only Pumba",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
